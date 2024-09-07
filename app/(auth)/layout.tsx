3e9c6ca486eae9dcb3a783167727008e37887b9b
IhorMalkov <igorzno2004@gmail.com>
2024-09-07 12:04:39 -0700

    Starting dev sign in form

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,16 @@
-export default function AuthRootLayout(){
+import {ReactNode} from "react";
+import GlassPane from "@/components/GlassPane";
+import "@/styles/global.css";
+
+export default function AuthRootLayout({children}: {children: ReactNode}) {
     return (
         <html lang="en">
-          <head/>
+           <head/>
+        <body className="h-screen w-screen h-screen rainbow-mesh p-6">
+        <GlassPane className="w-full h-full flex items-center justify-center">
+            {children}
+        </GlassPane>
+        </body>
         </html>
     )
 }
