e20c3ef93bb237d9a2282d10f74e0c776476ca74
IhorMalkov <igorzno2004@gmail.com>
2024-09-11 23:11:42 -0700

    Made a dashboard component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,15 @@
 import {ReactNode} from "react";
 import GlassPane from "@/components/GlassPane";
 import "@/styles/global.css";
+import Sidebar from "../../components/Sidebar";
 
 export default function AuthRootLayout({children}: {children: ReactNode}) {
     return (
         <html lang="en">
         <head/>
-        <body className="h-screen w-screen h-screen rainbow-mesh p-6">
+        <body className="h-screen w-screen h-screen candy-mesh p-6">
         <GlassPane className="w-full h-full flex items-center justify-center">
+            <Sidebar/>
             {children}
         </GlassPane>
         </body>
