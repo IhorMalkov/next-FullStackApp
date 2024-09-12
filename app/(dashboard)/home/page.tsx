52c3f03e0542a8acd10699e6ded1ffb4debdea2c
IhorMalkov <igorzno2004@gmail.com>
2024-09-12 13:26:12 -0700

    Added project component with dynamic routes

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -7,6 +7,7 @@ import { Suspense } from "react";
 import Greetings from "@/components/Greetings";
 import GreetingSkeleton from "@/components/GreetingSkeleton";
 import ProjectCard from "@/components/ProjectCard";
+import TaskCard from "@/components/TaskCard";
 
 const getData = async() => {
     await delay(2000);
@@ -35,14 +36,16 @@ export default async function Page() {
                 <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
                     {projects.map(project => (
                         <div className="w-1/3 p-3" key={project.id}>
-                            <Link href={`/projects/${project.id}`}>
+                            <Link href={`/project/${project.id}`}>
                                 <ProjectCard project={project} />
                             </Link>
                         </div>
                     ))}
                 </div>
                 <div className="mt-6 flex-2 grow w-full flex">
-                    <div className="w-full"></div>
+                    <div className="w-full">
+                        <TaskCard/>
+                    </div>
                 </div>
             </div>
         </div>
