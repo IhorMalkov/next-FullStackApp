5a31cb82f4842731911c848d3e4bd6d0949c106f
IhorMalkov <igorzno2004@gmail.com>
2024-09-11 23:37:17 -0700

    Starting to do project components, made a loader for my greetings component

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -5,12 +5,17 @@ import { cookies } from "next/headers";
 import Link from "next/link";
 import { Suspense } from "react";
 import Greetings from "@/components/Greetings";
+import GreetingSkeleton from "@/components/GreetingSkeleton";
 
 export default async function Page() {
     return (
-        <div className="h-full overflow-y-auto pr-6 w-1/1">
+        <div className="h-full overflow-y-auto pr-6 w-full">
             <div className=" h-full  items-stretch justify-center min-h-[content]">
-                <div className="flex-1 grow flex"><Greetings/></div>
+                <div className="flex-1 grow flex">
+                    <Suspense fallback={<GreetingSkeleton/>}>
+                        <Greetings/>
+                    </Suspense>
+                </div>
                 <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
                     <div className="w-1/3 p-3"></div>
                 </div>
