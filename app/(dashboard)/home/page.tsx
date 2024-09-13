c5b5911f2b4f1c6ef96d101308f0f45a64afe9c5
IhorMalkov <igorzno2004@gmail.com>
2024-09-13 13:20:43 -0700

    Add project functionallity

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -8,6 +8,7 @@ import Greetings from "@/components/Greetings";
 import GreetingSkeleton from "@/components/GreetingSkeleton";
 import ProjectCard from "@/components/ProjectCard";
 import TaskCard from "@/components/TaskCard";
+import NewProject from "@/components/NewProject";
 
 const getData = async() => {
     await delay(2000);
@@ -36,11 +37,14 @@ export default async function Page() {
                 <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
                     {projects.map(project => (
                         <div className="w-1/3 p-3" key={project.id}>
-                            <Link href={`/project /${project.id}`}>
-                                <ProjectCard project={project} />
+                            <Link href={`/project/${project.id}`}>
+                                <ProjectCard project={project}/>
                             </Link>
                         </div>
                     ))}
+                    <div className="w-1/3 p-3">
+                        <NewProject/>
+                    </div>
                 </div>
                 <div className="mt-6 flex-2 grow w-full flex">
                     <div className="w-full">
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -12,6 +12,7 @@ export default function AuthRootLayout({children}: {children: ReactNode}) {
             <Sidebar/>
             {children}
         </GlassPane>
+        <div id="modal"></div>
         </body>
         </html>
     )
